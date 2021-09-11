f019a8d3e5d2f7d0092aa7f52834070d10730c53
MohanKesappagari-careator <mohan.k@careator.com>
2021-09-11 23:59:38 +0530

    health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
+import { HealthController } from './health.controller';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { HealthCheckService, TerminusModule } from '@nestjs/terminus';
 
 @Module({
-  imports: [AuthModule, UserModule, TypeOrmModule.forRoot({})],
-  controllers: [AppController],
+  imports: [AuthModule, UserModule, TypeOrmModule.forRoot({}), TerminusModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
