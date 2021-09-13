ab289e78f77047d3bbb7cc2858f133d96215b8b3
MohanKesappagari-careator <mohan.k@careator.com>
2021-09-13 11:59:34 +0530

    typeorm session

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { HealthCheckService, TerminusModule } from '@nestjs/terminus';
-
 @Module({
   imports: [AuthModule, UserModule, TypeOrmModule.forRoot({}), TerminusModule],
   controllers: [AppController, HealthController],
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import { getRepository } from 'typeorm';
+import { TypeOrmSession } from './user/entities/session.entity';
+import { TypeormStore } from 'connect-typeorm/out';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const sessionRepo = getRepository(TypeOrmSession);
   app.use(
     session({
       secret: 'secret',
       resave: false,
       saveUninitialized: false,
       cookie: { maxAge: 36000000 },
+      store: new TypeormStore().connect(sessionRepo),
     }),
   );
   app.use(passport.initialize());
